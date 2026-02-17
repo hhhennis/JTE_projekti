a8ba42737bf243299655b6701e881ff69bc94faf
hhhennis <hmkkortelainen@gmail.com>
2026-02-17 21:05:46 +0200

    Varaus ttd red + green

---
+++ src/main/java/hellocucumber/Varaus.java
@@ -0,0 +1,9 @@
+package hellocucumber;
+
+public class Varaus {
+
+    public String varaa(String huone, String tila) {
+        return "Varaus onnistui";
+    }
+
+}
---
+++ src/test/java/hellocucumber/VarausTest.java
@@ -0,0 +1,16 @@
+package hellocucumber;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VarausTest {
+
+    @Test
+    void varausOnnistuu() {
+        Varaus varaus = new Varaus();
+
+        String varauksenTulos = varaus.varaa("A101", "vapaa");
+
+        assertEquals("Varaus onnistui", varauksenTulos);
+    }
+}
