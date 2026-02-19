e02577e7905ffbf647529251ee76bc3c03b552d0
hhhennis <hmkkortelainen@gmail.com>
2026-02-19 21:43:12 +0200

    varaukseen lisää yksikkötestejä, red-green

--- src/main/java/hellocucumber/Varaus.java
+++ src/main/java/hellocucumber/Varaus.java
@@ -2,7 +2,16 @@ package hellocucumber;
 
 public class Varaus {
 
-    public String varaa(String huone, String tila) {
+    public String varaa(String huone, String aika, String tila) {
+        if((huone==null)||(huone=="")){
+            if((aika==null)||(aika=="")){
+                return "Valitse huone ja varausajankohta";
+            }
+            return "Valitse huone";
+        }
+        if((aika==null)||(aika=="")){
+            return "Valitse varausajankohta";
+        }
         if(tila=="vapaa"){
             return "Varaus onnistui";
         } else {
--- src/test/java/hellocucumber/VarausTest.java
+++ src/test/java/hellocucumber/VarausTest.java
@@ -6,22 +6,77 @@ import static org.junit.jupiter.api.Assertions.*;
 public class VarausTest {
 
     @Test
-    void varausOnnistuu() {
+    void varaus_Onnistuu() {
         Varaus varaus = new Varaus();
 
-        String varauksenTulos = varaus.varaa("A101", "vapaa");
+        String varauksenTulos = varaus.varaa("A101", "2026-02-28","vapaa");
 
         assertEquals("Varaus onnistui", varauksenTulos);
     }
 
     @Test
-    void varausEpaonnistuu() {
+    void varaus_Epaonnistuu_Kun_TilaVarattu() {
         Varaus varaus = new Varaus();
 
-        String varauksenTulos = varaus.varaa("A101", "varattu");
+        String varauksenTulos = varaus.varaa("A101", "26-03-01","varattu");
 
         assertEquals("Huone ei ole vapaa", varauksenTulos);
     }
 
+    @Test
+    void varaus_Epaonnistuu_Kun_HuonePuuttuu() {
+        Varaus varaus = new Varaus();
 
+        String varauksenTulos = varaus.varaa("", "26-03-01",null);
+
+        assertEquals("Valitse huone", varauksenTulos);
+    }
+
+    @Test
+    void varaus_Epaonnistuu_Kun_AikaPuuttuu() {
+        Varaus varaus = new Varaus();
+
+        String varauksenTulos = varaus.varaa("A101", "",null);
+
+        assertEquals("Valitse varausajankohta", varauksenTulos);
+    }
+
+    @Test
+    void varaus_Epaonnistuu_Kun_HuoneJaAikaPuuttuu() {
+        Varaus varaus = new Varaus();
+
+        String varauksenTulos = varaus.varaa("", "",null);
+
+        assertEquals("Valitse huone ja varausajankohta", varauksenTulos);
+    }
+
+    @Test
+    void varaus_Epaonnistuu_Kun_HuoneNull() {
+        Varaus varaus = new Varaus();
+
+        String varauksenTulos = varaus.varaa(null, "26-03-01",null);
+
+        assertEquals("Valitse huone", varauksenTulos);
+    }
+
+    @Test
+    void varaus_Epaonnistuu_Kun_AikaNull() {
+        Varaus varaus = new Varaus();
+
+        String varauksenTulos = varaus.varaa("A101", null,null);
+
+        assertEquals("Valitse varausajankohta", varauksenTulos);
+    }
+
+    @Test
+    void varaus_Epaonnistuu_Kun_HuoneJaAikaNull() {
+        Varaus varaus = new Varaus();
+
+        String varauksenTulos = varaus.varaa(null, null, null);
+
+        assertEquals("Valitse huone ja varausajankohta", varauksenTulos);
+
+
+    }
 }
+
